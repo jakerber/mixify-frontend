01353e41c83f7de73ecdcac2303257c2ce497256
Josh Kerber <jakerber96@gmail.com>
2023-06-26 11:11:13 -0400

    Move Spotify client ID to env var

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export const HomePage = () => {
     const navigate = useNavigate();
     const [queueCode, setQueueCode] = useState('');
     let spotifyAuthUrl = `https://accounts.spotify.com/authorize?`;
-    spotifyAuthUrl += `client_id=a4ecbf8a741d49fcaa750f25f6ed1486`;
+    spotifyAuthUrl += `client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
     spotifyAuthUrl += `&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}`;
     spotifyAuthUrl += `&response_type=token&show_dialogue=true`;
     spotifyAuthUrl += `&scope=user-modify-playback-state%20`
